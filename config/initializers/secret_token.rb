db78c83edc8bb2da577e365c8eaea385e0cf5b0a
lightyrs <harris@harrisnovick.com>
2011-08-29 00:41:52 -0400

    rails new feng-shui

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FengShui::Application.config.secret_token = '9b6437f228f6d07f81ade8de1348bc995bb2c1e188494ac045d377f0b9b7f09107aee8e7d75cbec5d164c7e7a15a6e39497322eed11043a5d5eeff71a48beb56'
